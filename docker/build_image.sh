a63b79b2f114b91ca5cbeea0338534665e2876d2
Richard Jamieson <richard.jamieson@sscsuk.net>
2019-06-20 18:41:07 +0100

    updated version numbner - start 1.0

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,1 +1,1 @@
-docker build -t localhost:5000/scsuk.net/scs_httpd:2.4.6-89 -f docker/Dockerfile https://gitlab.scsuk.net/rich/scs_httpd.git#master
+docker build -t localhost:5000/scsuk.net/scs_httpd:1.0 -f docker/Dockerfile https://gitlab.scsuk.net/rich/scs_httpd.git#master
