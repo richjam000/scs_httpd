6fa223e45b73d9bfbfa385eefcfd75d1654aa1a0
Richard Jamieson <richard.jamieson@scsuk.net>
2019-08-01 23:28:52 +0100

    restructure to use gitlab registry and general tidy-up

---
+++ build_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+TAG="1.0"
+IMAGE="gitlab.scsuk.net:5005/scsuk/scs_httpd:$TAG"
+set -x
+cp /etc/yum.repos.d/scs_repo.repo yum.repos.d
+docker build -t "$IMAGE" .
+docker push "$IMAGE"
+set +x
+
--- docker/build_image.sh
+++
@@ -1,2 +0,0 @@
-docker build -t localhost:5000/scsuk.net/scs_httpd:1.0 -f docker/Dockerfile https://gitlab.scsuk.net/scsuk/scs_httpd.git#master
-docker push localhost:5000/scsuk.net/scs_httpd:1.0
